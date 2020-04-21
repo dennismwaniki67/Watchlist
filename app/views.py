9bb4c167fb81065aa5c74d34dc05c6a4a54aadf7
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-21 17:12:25 +0300

    create a search view function that has passes in a dynamic variable.

--- app/views.py
+++ app/views.py
@@ -30,3 +30,14 @@ def movie(id):
     title = f'{movie.title}'
 
     return render_template('movie.html',title = title,movie = movie)
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
