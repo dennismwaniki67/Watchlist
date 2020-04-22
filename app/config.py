176a00021384b4c5323ba1d2c5d7e649ba7a2c35
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-22 14:38:06 +0300

    added reviews

--- app/config.py
+++ app/config.py
@@ -3,17 +3,15 @@ class Config:
     General configuration parent class
     '''
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    
 
 
 
 class ProdConfig(Config):
     '''
-    Production  configuration child class
+    Pruduction  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
 
 
 class DevConfig(Config):
--- app/movie_test.py
+++ app/movie_test.py
@@ -1,4 +1,4 @@
-import unittest  
+import unittest
 from models import movie
 Movie = movie.Movie
 
@@ -6,15 +6,15 @@ class MovieTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Movie class
     '''
+
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234, 'Python Must Be Crazy', 'A thrilling new Python Series',
-                               'https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_movie,Movie))   # isinstance function checks if the object self.movie is an instance of the Movie class
+        self.assertTrue(isinstance(self.new_movie,Movie))
 
 
 if __name__ == '__main__':
